6dadfcdc69d72623cdc2f379b09e4625f6e39393
ByteManager <qorj24tpe305qaf@gmail.com>
2022-12-24 01:20:04 +0530

    the executable generated by running link.sh is now named 'Bytetoch'

--- link.sh
+++ link.sh
@@ -1,1 +1,1 @@
-gcc bin/main.o bin/argument.o bin/buffer.o bin/choice.o bin/close.o bin/error.o bin/output.o bin/terminal.o -o bin/binread
+gcc bin/main.o bin/argument.o bin/buffer.o bin/choice.o bin/close.o bin/error.o bin/output.o bin/terminal.o -o bin/Bytetoch
