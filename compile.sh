4d55b9f71cfd24e7b166f2589b67fab12e7f5448
ByteManager <qorj24tpe305qaf@gmail.com>
2022-12-23 22:31:19 +0530

    useful scripts

---
+++ compile.sh
@@ -0,0 +1,8 @@
+gcc -c argument.c
+gcc -c buffer.c
+gcc -c choice.c
+gcc -c close.c
+gcc -c error.c
+gcc -c output.c
+gcc -c terminal.c
+gcc -c main.c
---
+++ link.sh
@@ -0,0 +1,1 @@
+gcc main.o argument.o buffer.o choice.o close.o error.o output.o terminal.o -o bin
